3149963f9d7ca15cbbffdaddcc227e5e4857e5f0
Daniel Hazelton <dshadowwolf@gmail.com>
2019-12-11 13:13:36 -0500

    this is almost ready for the first test to be tried

--- src/main/java/com/mcmoddev/multiblocktest/util/IMultiBlockStructure.java
+++ src/main/java/com/mcmoddev/multiblocktest/util/IMultiBlockStructure.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public interface IMultiBlockStructure {
-	public boolean detectMultiblock(BlockPos base, World worldIn);
-	public Pair<BlockPos, BlockPos> findLimits(BlockPos base, World worldIn);
-	public List<Pair<BlockPos, IBlockState>> getContents(World worldIn);
+	public boolean detectMultiblock();
+	public Pair<BlockPos, BlockPos> findLimits();
+	public List<Pair<BlockPos, IBlockState>> getContents();
+	public List<TileEntity> getTiles();
+	public boolean form();
 }
--- src/main/java/com/mcmoddev/multiblocktest/util/MultiBlockStructure.java
+++ src/main/java/com/mcmoddev/multiblocktest/util/MultiBlockStructure.java
@@ -8,13 +8,15 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
+
 import org.apache.commons.lang3.tuple.MutableTriple;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
@@ -29,8 +31,12 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
 	private final MutableTriple<Integer,Integer,Integer> detected = MutableTriple.of(0, 0, 0);
 	private BlockPos minPos = new BlockPos(0,0,0);
 	private BlockPos maxPos = new BlockPos(0,0,0);
+	private BlockPos origin;
+	private World world;
 	
-	public MultiBlockStructure(int xWidth, int zWidth, int height, List<IBlockState> validBlocks) {
+	public MultiBlockStructure(BlockPos origin, World worldIn, int xWidth, int zWidth, int height, List<IBlockState> validBlocks) {
+		this.origin = origin;
+		this.world = worldIn;
 		this.xWidth = xWidth;
 		this.zWidth = zWidth;
 		this.height = height;
@@ -60,6 +66,16 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
 	 */
 	abstract boolean isValidMultiblock(IBlockState origin);
 
+	@Override
+	public List<TileEntity> getTiles() {
+		if (minPos != null && maxPos != null) {
+			return StreamSupport.stream(BlockPos.getAllInBox(minPos, maxPos).spliterator(), false)
+					.map(p -> world.getTileEntity(p))
+					.filter(p -> p != null).collect(Collectors.toList());
+		}
+		return Collections.<TileEntity>emptyList();		
+	}
+	
 	/**
 	 * Assuming a cuboid multiblock this will first seek out the actual extents of the possible structure,
 	 * then walk the blocks in that region. If it finds only valid blocks, it will return true, otherwise it will
@@ -71,11 +87,11 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
 	 * @return whether the multiblock has been detected or not
 	 */
 	@Override
-	public boolean detectMultiblock(BlockPos base, World worldIn) {
-		Pair<BlockPos, BlockPos> ext = findLimits(base, worldIn);
+	public boolean detectMultiblock() {
+		Pair<BlockPos, BlockPos> ext = findLimits();
 		List<IBlockState> blocks = new LinkedList<>();
 		
-		BlockPos.getAllInBox(ext.getLeft(), ext.getRight()).forEach( pos -> blocks.add(worldIn.getBlockState(pos)) );
+		BlockPos.getAllInBox(ext.getLeft(), ext.getRight()).forEach( pos -> blocks.add(world.getBlockState(pos)) );
 	
 		return validBlocks.containsAll(blocks);
 	}
@@ -90,18 +106,18 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
 	 * @return Pair<BlockPos, BlockPos> - The minimum (left value) and maximum (right value) extents
 	 */
 	@Override
-	public Pair<BlockPos, BlockPos> findLimits(BlockPos base, World worldIn) {
+	public Pair<BlockPos, BlockPos> findLimits() {
         final Queue<BlockPos> remainingCandidates = new ArrayDeque<>();
         final Set<BlockPos> visited = new HashSet<>();
         BlockPos.MutableBlockPos min = new BlockPos.MutableBlockPos();
         BlockPos.MutableBlockPos max = new BlockPos.MutableBlockPos();
 
-        remainingCandidates.add(base);
+        remainingCandidates.add(origin);
         while(!remainingCandidates.isEmpty())
         {
             BlockPos next = remainingCandidates.remove();
             visited.add(next);
-            if (next == base || validBlocks.contains(worldIn.getBlockState(next)))
+            if (next == origin || validBlocks.contains(world.getBlockState(next)))
             {
                 min.setPos(
                         Math.min(min.getX(),next.getX()),
@@ -117,9 +133,9 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
                 {
                     BlockPos newCandidate = next.offset(f);
                     if (!visited.contains(newCandidate) &&
-                            Math.abs(newCandidate.getX()-base.getX()) <= xWidth &&
-                            Math.abs(newCandidate.getY()-base.getY()) <= height &&
-                            Math.abs(newCandidate.getZ()-base.getZ()) <= zWidth
+                            Math.abs(newCandidate.getX()-origin.getX()) <= xWidth &&
+                            Math.abs(newCandidate.getY()-origin.getY()) <= height &&
+                            Math.abs(newCandidate.getZ()-origin.getZ()) <= zWidth
                         )
                     {
                         remainingCandidates.add(newCandidate);
@@ -145,12 +161,13 @@ public abstract class MultiBlockStructure implements IMultiBlockStructure {
 	 * @return List of Pair<BlockPos, IBlockState> describing the blocks in the interior and their contents
 	 */
 	@Override
-	public List<Pair<BlockPos, IBlockState>> getContents(World worldIn) {
+	public List<Pair<BlockPos, IBlockState>> getContents() {
 		if (minPos != null && maxPos != null) {
-			List<Pair<BlockPos, IBlockState>> rv = new LinkedList<>();
 			Vec3i addSub = new Vec3i(1,1,1);
-			BlockPos.getAllInBox(minPos.add(addSub), maxPos.subtract(addSub)).forEach(pos -> rv.add(Pair.of(pos, worldIn.getBlockState(pos))));
-			return rv.stream().filter(pp -> pp.getRight().getBlock() != null && pp.getRight().getBlock() != Blocks.AIR).collect(Collectors.toList());
+			return StreamSupport.stream(BlockPos.getAllInBox(minPos.add(addSub), maxPos.subtract(addSub)).spliterator(), false)
+					.map(p -> Pair.of(p, world.getBlockState(p)))
+					.filter(p -> p.getRight() != null && p.getRight().getBlock() != null)
+					.collect(Collectors.toList());
 		}
 		return Collections.<Pair<BlockPos,IBlockState>>emptyList();
 	}
