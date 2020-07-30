f4c7d34222f727b6d13c541302aad2891b174b47
Daniel Hazelton <dshadowwolf@gmail.com>
2020-07-30 10:21:37 -0400

    actually make the new cap registered

--- src/main/java/com/mcmoddev/multiblocktest/init/MyBlocks.java
+++ src/main/java/com/mcmoddev/multiblocktest/init/MyBlocks.java
@@ -33,13 +33,15 @@ public final class MyBlocks {
     public static final Block BLOCK_BANK_CONTROLLER   = new CapBankController(Material.IRON);
     public static final Block BLOCK_BANK_INPUT        = new CapBankInputJack(Material.IRON);
     public static final Block BLOCK_BANK_OUTPUT       = new CapBankOutputJack(Material.IRON);
+    public static final Block CREATIVE_CAPACITOR      = new CapBankCreative(Material.IRON);
     
     public static final Item[] cap_items = { new ItemBlock(CAPACITOR_BANK_BASE).setRegistryName(CAPACITOR_BANK_BASE.getRegistryName()), 
     		new ItemBlock(CAPACITOR_BANK_ADVANCED).setRegistryName(CAPACITOR_BANK_ADVANCED.getRegistryName()),
     		new ItemBlock(CAPACITOR_BANK_MASSIVE).setRegistryName(CAPACITOR_BANK_MASSIVE.getRegistryName()),
-    		new ItemBlock(CAPACITOR_BANK_FINAL).setRegistryName(CAPACITOR_BANK_FINAL.getRegistryName()) };
-    public static final Block[] capacitors = { CAPACITOR_BANK_BASE,  CAPACITOR_BANK_ADVANCED, CAPACITOR_BANK_MASSIVE, CAPACITOR_BANK_FINAL };
-    public static final Block[] allBlocks = { CAPACITOR_BANK_BASE,  CAPACITOR_BANK_ADVANCED, CAPACITOR_BANK_MASSIVE, CAPACITOR_BANK_FINAL, BLOCK_BANK_CONTROLLER, BLOCK_BANK_INPUT, BLOCK_BANK_OUTPUT};
+    		new ItemBlock(CAPACITOR_BANK_FINAL).setRegistryName(CAPACITOR_BANK_FINAL.getRegistryName()),
+    		new ItemBlock(CREATIVE_CAPACITOR).setRegistryName(CREATIVE_CAPACITOR.getRegistryName())};
+    public static final Block[] capacitors = { CAPACITOR_BANK_BASE,  CAPACITOR_BANK_ADVANCED, CAPACITOR_BANK_MASSIVE, CAPACITOR_BANK_FINAL, CREATIVE_CAPACITOR };
+    public static final Block[] allBlocks = { CAPACITOR_BANK_BASE,  CAPACITOR_BANK_ADVANCED, CAPACITOR_BANK_MASSIVE, CAPACITOR_BANK_FINAL, BLOCK_BANK_CONTROLLER, BLOCK_BANK_INPUT, BLOCK_BANK_OUTPUT, CREATIVE_CAPACITOR};
     public static final Item[] mbItems = Arrays.asList(BLOCK_BANK_CONTROLLER, BLOCK_BANK_INPUT, BLOCK_BANK_OUTPUT).stream()
     		.map(b -> new ItemBlock(b).setRegistryName(b.getRegistryName())).collect(Collectors.toCollection(Vector::new)).toArray(new Item[3]);
 }
